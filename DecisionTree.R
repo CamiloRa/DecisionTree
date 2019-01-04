463b94ff9e2e552c6675288187b248e983d2a53b
CamiloRa <camilor@gmail.com>
2019-01-03 20:06:31 -0500

    Update DecisionTree.R
    Libraries. 
    Needs objective yet.

--- DecisionTree.R
+++ DecisionTree.R
@@ -1,7 +1,12 @@
 #DecisionTree.R
 # working with trasf_ld as distance dataframe
 
-semilla <- hclust(distance_trasf_ld,method = "ward.D")
+
+library(ggplot)
+library(tree)
+  
+  
+semilla <- hclust(distance_trasf_ld, method = "ward.D")
 
 plot(semilla, hang = -1)
-plot(space
+
