b3278dceee1fd47245a587d0355e84e06ab26b08
Camilo Ramirez <camilor@gmail.com>
2019-01-04 21:07:52 -0500

    Fixed library function Loaddata

--- DecisionTree.R
+++ DecisionTree.R
@@ -2,10 +2,12 @@
 # working with trasf_ld as distance dataframe
 
 
-library(ggplot)
-library(tree)
+library(ggplot2)
+library(cluster)  
   
-  
+
+data(pluton)
+
 semilla <- hclust(distance_trasf_ld, method = "ward.D")
 
 plot(semilla, hang = -1)
